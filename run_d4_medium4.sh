5f987ef8b55748c913d852a20168b82aad362f72
baloghAndi <a.balogh@cs.ucc.ie>
2024-08-15 19:51:19 +0000

    up

--- run_d4_medium4.sh
+++ run_d4_medium4.sh
@@ -3,14 +3,16 @@
 
 seed=1234
 if [[  $1 = "1" ]]  ; then
-      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" '04_iscas89_s1494_bench.cnf' 'hybrid_wmc' 'dynamic' $1
+      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" './input/Dataset_preproc/04_iscas89_s1494_bench.cnf' 'hybrid_wmc' 'dynamic' $1
 elif [[  $1 = "2" ]]  ; then
-      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" '04_iscas89_s820_bench.cnf' 'hybrid_wmc' 'dynamic' $1
+      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" './input/Dataset_preproc/04_iscas89_s820_bench.cnf' 'hybrid_wmc' 'dynamic' $1
 elif [[  $1 = "3" ]]  ; then
-      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" '04_iscas89_s832_bench.cnf' 'hybrid_wmc' 'dynamic' $1
+      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" './input/Dataset_preproc/04_iscas89_s832_bench.cnf' 'hybrid_wmc' 'dynamic' $1
 elif [[  $1 = "4" ]]  ; then
-      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" '04_iscas89_s953_bench.cnf' 'hybrid_wmc' 'dynamic' $1
+      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" './input/Dataset_preproc/04_iscas89_s953_bench.cnf' 'hybrid_wmc' 'dynamic' $1
 elif [[  $1 = "5" ]]  ; then
-      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" '05_iscas93_s967_bench.cnf' 'hybrid_wmc' 'dynamic' $1
-elif
+      timeout 21600 python3 greedy_selective_backboneD4.py "./input/Dataset_preproc/" './input/Dataset_preproc/05_iscas93_s967_bench.cnf' 'hybrid_wmc' 'dynamic' $1
+else
+	echo "wrong"
+fi
 
