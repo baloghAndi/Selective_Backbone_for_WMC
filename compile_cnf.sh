6815a2c79f6a11da73a1c18cf7dde3bf50f7e8a7
baloghAndi <a.balogh@cs.ucc.ie>
2024-08-15 14:15:30 +0000

    up

--- compile_cnf.sh
+++ compile_cnf.sh
@@ -10,23 +10,23 @@ for i in $folder*.cnf; do
       	      if  [[   $i = *"/03_"* ]] ||  [[   $i = *"/05_"* ]] || [[   $i = *"/06_"* ]] ||  [[   $i = *"/16_"* ]]  ;  then
 	              timeout 3600 python3 compile_cnf.py $i $1
 	            fi
-	          fi
-	          if [[  $3 = "2" ]]  ; then
-      	      if [[   $i = *"/07_"* ]]  ; then
+	 fi
+	if [[  $3 = "2" ]]  ; then
+      	     if [[   $i = *"/07_"* ]]  ; then
 	              timeout 3600 python3 compile_cnf.py $i $1
-	    s        fi
+	            fi
 	          fi
-	          if [[  $3 = "3" ]]  ; then
+	 if [[  $3 = "3" ]]  ; then
       	      if [[   $i = *"/13_"* ]]  ; then
 	             timeout 3600 python3 compile_cnf.py $i $1
 	            fi
-	          fi
-	          if [[  $3 = "4" ]]  ; then
+	  fi
+	  if [[  $3 = "4" ]]  ; then
       	      if [[   $i = *"/15_"* ]]  ; then
 	              timeout 3600 python3 compile_cnf.py $i $1
 	            fi
 	      fi
 
-      fi
+     fi
     #fi
 done
