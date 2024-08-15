0eac25902cd7f94303c70a3663e87d83b089cf2e
Andrea <bal.andrea.ogh@gmail.com>
2024-08-15 15:05:58 +0100

    run partial SB files to compile

--- compile_cnf.sh
+++ compile_cnf.sh
@@ -2,27 +2,27 @@
 
 folder="./input/Dataset_preproc/"
 for i in $folder*.cnf; do
-    if [[ $i = *"temphybrid_wmcdynamic_22percent"* ]] ; then
+    if [[ $i = *"temphybrid_wmcdynamic_partialSB"* ]] ; then
       echo $i
-      foldername="${i%%"temphybrid_wmcdynamic_22percent"*}"".cnf"
+      foldername="${i%%"temphybrid_wmcdynamic_partialSB"*}"".cnf"
       filename="${foldername##*/}"
       if [[  $3 = "1" ]]  ; then
-      	      if  [[   $i = *"/03_"* ]] ||  [[   $i = *"/04_"* ]] || [[   $i = *"/05_"* ]] ||  [[   $i = *"/06_"* ]]  ||  [[   $i = *"/07_"* ]] ||  [[   $i = *"/08_"* ]] ||  [[   $i = *"/09_"* ]] ;  then
+      	      if  [[   $i = *"/03_"* ]] ||  [[   $i = *"/05_"* ]] || [[   $i = *"/06_"* ]] ||  [[   $i = *"/16_"* ]]  ;  then
 	              timeout 3600 python3 compile_cnf.py $i $1
 	            fi
 	          fi
 	          if [[  $3 = "2" ]]  ; then
-      	      if [[   $i = *"/10_"* ]] || [[   $i = *"/11_"* ]] || [[   $i = *"/12_"* ]]  ; then
+      	      if [[   $i = *"/07_"* ]]  ; then
 	              timeout 3600 python3 compile_cnf.py $i $1
 	    s        fi
 	          fi
 	          if [[  $3 = "3" ]]  ; then
-      	      if [[   $i = *"/13_"* ]] || [[   $i = *"/14_"* ]] ; then
+      	      if [[   $i = *"/13_"* ]]  ; then
 	             timeout 3600 python3 compile_cnf.py $i $1
 	            fi
 	          fi
 	          if [[  $3 = "4" ]]  ; then
-      	      if [[   $i = *"/16_"* ]] || [[   $i = *"/15_"* ]] ; then
+      	      if [[   $i = *"/15_"* ]]  ; then
 	              timeout 3600 python3 compile_cnf.py $i $1
 	            fi
 	      fi
