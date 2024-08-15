deffd5d1be8c8dfcccef214d88390181b78583dd
baloghAndi <a.balogh@cs.ucc.ie>
2024-08-15 16:13:37 +0000

    add partial compilations

--- compile_cnf.sh
+++ compile_cnf.sh
@@ -6,22 +6,22 @@ for i in $folder*.cnf; do
       echo $i
       foldername="${i%%"temphybrid_wmcdynamic_partialSB"*}"".cnf"
       filename="${foldername##*/}"
-      if [[  $3 = "1" ]]  ; then
+      if [[  $1 = "1" ]]  ; then
       	      if  [[   $i = *"/03_"* ]] ||  [[   $i = *"/05_"* ]] || [[   $i = *"/06_"* ]] ||  [[   $i = *"/16_"* ]]  ;  then
 	              timeout 3600 python3 compile_cnf.py $i $1
 	            fi
 	 fi
-	if [[  $3 = "2" ]]  ; then
+	if [[  $1 = "2" ]]  ; then
       	     if [[   $i = *"/07_"* ]]  ; then
 	              timeout 3600 python3 compile_cnf.py $i $1
 	            fi
 	          fi
-	 if [[  $3 = "3" ]]  ; then
+	 if [[  $1 = "3" ]]  ; then
       	      if [[   $i = *"/13_"* ]]  ; then
 	             timeout 3600 python3 compile_cnf.py $i $1
 	            fi
 	  fi
-	  if [[  $3 = "4" ]]  ; then
+	  if [[  $1 = "4" ]]  ; then
       	      if [[   $i = *"/15_"* ]]  ; then
 	              timeout 3600 python3 compile_cnf.py $i $1
 	            fi
